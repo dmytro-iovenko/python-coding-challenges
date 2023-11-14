4a89e3d5b118d2c555310e867b8352b272fde7e2
liudmylai <liudmyla.iovenko@gmail.com>
2023-11-13 20:33:42 -0500

    print the character at a specific index

--- 01-string-basic.py
+++ 01-string-basic.py
@@ -1,3 +1,15 @@
+# Print the Length of a String
 s = "Python"
-
 print(len(s))
+
+# Print the Character at a Specific Index
+s = "Hello"
+i = 0
+
+if not s:
+    print("Empty String")
+elif i < len(s):
+    print(s[i])
+else:
+    print("i out of range")
+
