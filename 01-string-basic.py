f00fa1310fa3df7582f916a5b5e9efdb72a2204c
liudmylai <liudmyla.iovenko@gmail.com>
2023-11-15 21:37:41 -0500

    print first and last three characters

--- 01-string-basic.py
+++ 01-string-basic.py
@@ -1,8 +1,8 @@
-# Print the Length of a String
+# Print the length of a string
 s = "Python"
 print(len(s))
 
-# Print the Character at a Specific Index
+# Print the character at a specific index
 s = "Hello"
 i = 0
 
@@ -13,8 +13,18 @@ elif i < len(s):
 else:
     print("i out of range")
 
-# Reverse a String
+# Reverse a string
 s = "Hello"
 
 reversed_word = s[::-1]
 print(reversed_word)
+
+# Print first and last three characters of a string
+s = "Wonderful"
+num_chars = 3
+
+if len(s) < 6:
+    print("")
+else:
+    new_string = s[:num_chars] + s[len(s)-num_chars:]
+    print(new_string)
