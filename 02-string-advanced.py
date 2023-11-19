31c522072036d09d13b96ea9ff8324d80aacb270
liudmylai <liudmyla.iovenko@gmail.com>
2023-11-18 22:47:21 -0500

    check if string contains all letters

--- 02-string-advanced.py
+++ 02-string-advanced.py
@@ -4,3 +4,16 @@ COMMA = ","
 DOT = "."
 
 print(s.replace(COMMA, DOT))
+
+# Check if string contains all letters in the alphabet
+import string
+
+s = "The quick brown fox jumps over the lazy dog"
+is_pangram = True
+
+for char in string.ascii_lowercase:
+    if char not in s.lower():
+        is_pangram = False
+        break # Stop the loop immediately
+
+print(is_pangram)
