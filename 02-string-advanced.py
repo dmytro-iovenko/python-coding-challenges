35f13281f3243c191bf17744a88e8a20d4cf96ba
liudmylai <liudmyla.iovenko@gmail.com>
2023-11-23 18:06:45 -0500

    count repeated characters

--- 02-string-advanced.py
+++ 02-string-advanced.py
@@ -49,3 +49,16 @@ for word in words_list:
 new_s.rstrip()
 
 print(new_s)
+
+# Count repeated characters
+s = "Hello"
+
+repeated_count = 0
+repeated_chars = []
+
+for char in s:
+	if (s.count(char) > 1) and (char not in repeated_chars):
+		repeated_count += 1
+		repeated_chars.append(char)
+
+print(repeated_count)
