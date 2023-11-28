2ae7b163f1bceccb0bda21e1053d2f9775cad4b8
liudmylai <liudmyla.iovenko@gmail.com>
2023-11-27 23:26:54 -0500

    get max and min values

--- 03-lists-and-tuples.py
+++ 03-lists-and-tuples.py
@@ -12,3 +12,11 @@ my_list = [3, 4, 5, 6]
 
 for elem in my_list:
 	print(elem, end=" ")
+
+# Get max and min values
+my_list = [3, 4, 5, 6]
+
+if my_list:
+	print(max(my_list), min(my_list))
+else:
+	print(None)
