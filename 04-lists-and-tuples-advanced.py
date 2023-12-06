94c29222fd720d48c585f9b17b82910bfa8005b0
liudmylai <liudmyla.iovenko@gmail.com>
2023-12-05 21:40:31 -0500

    distance between two 3d points

--- 04-lists-and-tuples-advanced.py
+++ 04-lists-and-tuples-advanced.py
@@ -9,3 +9,17 @@ for elem in listA:
 		difference.append(elem)
 
 print(difference)
+
+# Distance between two 3d points
+import math
+
+pointA = [3, 4, 5]
+pointB = [1, 3, 5]
+
+addition = ((pointA[0] - pointB[0])**2
+			+ (pointA[1] - pointB[1])**2
+			+ (pointA[2] - pointB[2])**2)
+
+distance = math.sqrt(addition)
+
+print(distance)
